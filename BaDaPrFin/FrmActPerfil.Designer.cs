5253c41a985d2d6b9e8a832e98e04a4c4b91a2d1
LUIS ORLANDO ANDRADE LEMUS <Ls21120218@surguanajuato.tecnm.mx>
2023-11-04 16:20:53 -0600

    Se solucionaron Problemas de Codigo y se implementaron nuevas pantallas asi como la organizacion del codigo que ya teniamos presente.

---
+++ BaDaPrFin/FrmActPerfil.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace BaDaPrFin
+{
+    partial class FrmActPerfil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtConfirmarContrasena = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCVV = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFechaCaducidad = new System.Windows.Forms.TextBox();
+            this.lblNumeroTarjeta = new System.Windows.Forms.Label();
+            this.txtNumTarjeta = new System.Windows.Forms.TextBox();
+            this.txtApellidoMaterno = new System.Windows.Forms.TextBox();
+            this.lblApellidoMaterno = new System.Windows.Forms.Label();
+            this.lblApellidoPaterno = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblContrasena = new System.Windows.Forms.Label();
+            this.txtApellidoPaterno = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 13);
+            this.label4.TabIndex = 41;
+            this.label4.Text = "Confirmar Contraseña";
+            // 
+            // txtConfirmarContrasena
+            // 
+            this.txtConfirmarContrasena.Location = new System.Drawing.Point(123, 85);
+            this.txtConfirmarContrasena.Name = "txtConfirmarContrasena";
+            this.txtConfirmarContrasena.Size = new System.Drawing.Size(100, 20);
+            this.txtConfirmarContrasena.TabIndex = 40;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(429, 279);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(115, 36);
+            this.btnGuardar.TabIndex = 39;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(123, 257);
+            this.txtEdad.MaxLength = 3;
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(42, 20);
+            this.txtEdad.TabIndex = 38;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 260);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Edad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(458, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "CVV";
+            // 
+            // txtCVV
+            // 
+            this.txtCVV.Location = new System.Drawing.Point(492, 59);
+            this.txtCVV.MaxLength = 18;
+            this.txtCVV.Name = "txtCVV";
+            this.txtCVV.Size = new System.Drawing.Size(52, 20);
+            this.txtCVV.TabIndex = 35;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(266, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Fecha de Vencimineto";
+            // 
+            // txtFechaCaducidad
+            // 
+            this.txtFechaCaducidad.Location = new System.Drawing.Point(385, 59);
+            this.txtFechaCaducidad.MaxLength = 18;
+            this.txtFechaCaducidad.Name = "txtFechaCaducidad";
+            this.txtFechaCaducidad.Size = new System.Drawing.Size(52, 20);
+            this.txtFechaCaducidad.TabIndex = 33;
+            // 
+            // lblNumeroTarjeta
+            // 
+            this.lblNumeroTarjeta.AutoSize = true;
+            this.lblNumeroTarjeta.Location = new System.Drawing.Point(284, 37);
+            this.lblNumeroTarjeta.Name = "lblNumeroTarjeta";
+            this.lblNumeroTarjeta.Size = new System.Drawing.Size(95, 13);
+            this.lblNumeroTarjeta.TabIndex = 32;
+            this.lblNumeroTarjeta.Text = "Numero de Tarjeta";
+            // 
+            // txtNumTarjeta
+            // 
+            this.txtNumTarjeta.Location = new System.Drawing.Point(385, 33);
+            this.txtNumTarjeta.MaxLength = 18;
+            this.txtNumTarjeta.Name = "txtNumTarjeta";
+            this.txtNumTarjeta.Size = new System.Drawing.Size(159, 20);
+            this.txtNumTarjeta.TabIndex = 31;
+            // 
+            // txtApellidoMaterno
+            // 
+            this.txtApellidoMaterno.Location = new System.Drawing.Point(123, 231);
+            this.txtApellidoMaterno.Name = "txtApellidoMaterno";
+            this.txtApellidoMaterno.Size = new System.Drawing.Size(100, 20);
+            this.txtApellidoMaterno.TabIndex = 30;
+            // 
+            // lblApellidoMaterno
+            // 
+            this.lblApellidoMaterno.AutoSize = true;
+            this.lblApellidoMaterno.Location = new System.Drawing.Point(33, 231);
+            this.lblApellidoMaterno.Name = "lblApellidoMaterno";
+            this.lblApellidoMaterno.Size = new System.Drawing.Size(86, 13);
+            this.lblApellidoMaterno.TabIndex = 29;
+            this.lblApellidoMaterno.Text = "Apellido Materno";
+            // 
+            // lblApellidoPaterno
+            // 
+            this.lblApellidoPaterno.AutoSize = true;
+            this.lblApellidoPaterno.Location = new System.Drawing.Point(33, 201);
+            this.lblApellidoPaterno.Name = "lblApellidoPaterno";
+            this.lblApellidoPaterno.Size = new System.Drawing.Size(84, 13);
+            this.lblApellidoPaterno.TabIndex = 28;
+            this.lblApellidoPaterno.Text = "Apellido Paterno";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(70, 175);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 27;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblContrasena
+            // 
+            this.lblContrasena.AutoSize = true;
+            this.lblContrasena.Location = new System.Drawing.Point(21, 62);
+            this.lblContrasena.Name = "lblContrasena";
+            this.lblContrasena.Size = new System.Drawing.Size(96, 13);
+            this.lblContrasena.TabIndex = 26;
+            this.lblContrasena.Text = "Nueva Contraseña";
+            // 
+            // txtApellidoPaterno
+            // 
+            this.txtApellidoPaterno.Location = new System.Drawing.Point(123, 201);
+            this.txtApellidoPaterno.Name = "txtApellidoPaterno";
+            this.txtApellidoPaterno.Size = new System.Drawing.Size(100, 20);
+            this.txtApellidoPaterno.TabIndex = 25;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(123, 172);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 24;
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Location = new System.Drawing.Point(123, 59);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(100, 20);
+            this.txtContrasena.TabIndex = 23;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(123, 34);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 22;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(64, 37);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(43, 13);
+            this.lblUsuario.TabIndex = 21;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // FrmActPerfil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 323);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtConfirmarContrasena);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCVV);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFechaCaducidad);
+            this.Controls.Add(this.lblNumeroTarjeta);
+            this.Controls.Add(this.txtNumTarjeta);
+            this.Controls.Add(this.txtApellidoMaterno);
+            this.Controls.Add(this.lblApellidoMaterno);
+            this.Controls.Add(this.lblApellidoPaterno);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblContrasena);
+            this.Controls.Add(this.txtApellidoPaterno);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblUsuario);
+            this.Name = "FrmActPerfil";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmActPerfil";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Salir);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtConfirmarContrasena;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCVV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFechaCaducidad;
+        private System.Windows.Forms.Label lblNumeroTarjeta;
+        private System.Windows.Forms.TextBox txtNumTarjeta;
+        private System.Windows.Forms.TextBox txtApellidoMaterno;
+        private System.Windows.Forms.Label lblApellidoMaterno;
+        private System.Windows.Forms.Label lblApellidoPaterno;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblContrasena;
+        private System.Windows.Forms.TextBox txtApellidoPaterno;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblUsuario;
+    }
+}
---
+++ Modelos/Model_Usuario.cs
@@ -0,0 +1,53 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Model_Usuarios
+    {
+        public int ID { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido_Paterno { get; set; }
+        public String Apellido_Materno { get; set; }
+        public String Usuario { get; set; }
+        public String Contrasena { get; set; }
+        public String Numero_Tarjeta { get; set; }
+        public String Card_Expired { get; set; }
+        public String CVV { get; set; }
+
+        public Model_Usuarios(int id, string nombre, string app, string apm,
+            string user, string psw, string cardNum, string cardExp, string cvv)
+        {
+            ID = id;
+            Nombre = nombre;
+            Apellido_Paterno = app;
+            Apellido_Materno = apm;
+            Usuario = user;
+            Contrasena = psw;
+            Numero_Tarjeta = cardNum;
+            Card_Expired = cardExp;
+            CVV = cvv;
+        }
+
+        public Model_Usuarios() 
+        {
+            ID = 0;
+            Nombre = "";
+            Apellido_Paterno = "";
+            Apellido_Materno = "";
+            Usuario = "";
+            Contrasena = "";
+            Numero_Tarjeta = "";
+            Card_Expired = "";
+            CVV = "";
+        }
+
+
+    }
+
+}
